bb2f62b0f9cb49009d831b758a9df272c2051d28
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-08-17 23:49:43 +0700

    121. Persistence: TypeORM, MongoDB and our Lesson Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { Lesson } from './lesson/lesson.entity';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
